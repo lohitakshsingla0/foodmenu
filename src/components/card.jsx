aaf2e634dd32ca4c0789772c7fe359273066bc70
Lohitaksh Singla <lohitakshsingla0@gmail.com>
2022-09-27 20:41:12 +0200

    setting of cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -8,22 +8,25 @@ const CardApp=({cardData})=>{
     cardData.map((attribute) => (
       
       <div class="card">
+        
         <div class="card__header">
-          <img src={attribute.strCategoryThumb} alt="card__image" class="card__image" width="600"></img>
+          <img src={attribute.strCategoryThumb} alt="card__image" class="card__image" ></img>
         </div>
+
         <div class="card__body">
           <span class="tag tag-blue">Food</span>
           <h4>{attribute.strCategory}</h4>
           <p>{attribute.strCategoryDescription}</p>
         </div>
+
         <div class="card__footer">
           <div class="user">
             <div class="user__info">
               <h5>By Lohit</h5>
-              
             </div>
           </div>
         </div>
+
       </div>
      
    
